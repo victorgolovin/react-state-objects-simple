f65107f9e4f5d33e4effe765a6e7078c96ec6993
Victor Golovin <rinoally@gmail.com>
2023-09-25 12:55:25 +0300

    state-objects

--- src/BIO.jsx
+++ src/BIO.jsx
@@ -4,10 +4,25 @@ import { useState } from 'react'
 
 const BIO = () => {
 
+  const [person, setState] = useState({
+    name: 'victor',
+    surname: 'golovin',
+    age: '29',
+    sex: 'male',
+    interests: ['youtube', 'word', 'games']
+  });
+
   return (
-    <>
-      <p>1123</p>
-    </>
+    <div>
+      <p>Name: {person.name}</p>
+      <p>Surname: {person.surname}</p>
+      <p>Age: {person.age}</p>
+      <p>Sex: {person.sex}</p>
+      <p>Interest: </p>
+      <p>{person.interests.map(interest => (
+        <p key={interest}>{interest}</p>
+      ))}</p>
+    </div>
   )
 }
 
